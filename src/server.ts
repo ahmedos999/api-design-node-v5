ec32d3981c3d876316c8a5015f0890fb65b9a33e
Ahmed Sheikh <ahmedos099912@gmail.com>
2026-08-09 15:48:10 +0400

    middlewares

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,20 @@ import express from 'express'
 import authRoutes from './routes/authRoutes.ts'
 import userRoutes from './routes/userRoutes.ts'
 import habitRoutes from './routes/habitRoutes.ts'
+import cors from 'cors'
+import morgen from 'morgan'
+import helmet from 'helmet'
 
 const app = express()
+
+// external middelware
+app.use(helmet())
+app.use(cors())
+app.use(morgen('dev'))
+
+// express middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/health', (req, res) => {
   res.json({ message: 'all good' }).status(200)
